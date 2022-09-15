1c05407c6176d0fa3cb4c75959e587c950e87f45
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2022-09-15 23:44:21 +0200

    searching algorithm

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,34 +5,46 @@ require 'byebug'
 class Knight < Chess
   attr_accessor :position, :all_moves_available, :tree_of_moves, :board_of_nodes
   
-  def initialize(position)
-    @tree_of_moves = build_tree_of_moves
+  def initialize
     @@board = build_board
-    @@board_of_nodes = build_board_of_nodes
-    within_board?(position) ? @position = position : position = nil
-  end
-
-  def add_children_to_nodes
-    @board_of_nodes.each do |node|
-      all_subsequent_moves = all_moves_calculation(nodes.coordinates)
-      all_subsequent_moves.each do |coordinates|
-        @board_of_nodes.childrens << find_node_by_coordinates(coordinates)
-      end    
-    end
-  end
-
-  def find_node_by_coordinates(coordinates)
-    @@board_of_nodes.each do |node|
-      return node if node.coordinates == coordinates
-    end
   end
 
   def knight_moves(coordinates, destination)
     return nil unless within_board?(coordinates) || within_board?(destination)    
-    all_moves_calculation(coordinates)
+    queue_of_moves = []
+    queue_of_moves << coordinates
+    path_to_destination_raw = breadth_first_search(queue_of_moves, destination)
+    byebug
+    path_to_destination_nice = path_to_destination_processing(path_to_destination_raw)
+  end
+  
+  def path_to_destination_processing(raw_path)
+    raw_path = raw_path.flatten
+    path_to_return = []
+    until raw_path.empty?
+      ar = []
+      ar << raw_path[0]
+      ar << raw_path[1]
+      raw_path.shift
+      raw_path.shift
+      path_to_return << ar
+    end
+    byebug
+    path_to_return.reverse
   end
 
-  def all_moves_calculation(coordinates = @position)
+  def breadth_first_search(queue, destination)
+    until queue.empty?
+      parent = queue.first
+      return parent if parent[0] == destination[0] && parent[1] == destination[1] 
+      child_moves = all_moves_available(parent[0..1])
+      child_moves.each { |mov| mov << parent }
+      child_moves.each { |mov| queue << mov }
+      queue.shift
+    end
+  end
+
+  def all_moves_available(coordinates)
     basic_moves = [
       [1, 2],
       [2, 1],
@@ -44,16 +56,16 @@ class Knight < Chess
       [-2, -1]
     ]
 
-    all_moves_available = []
+    all_moves = []
     until basic_moves.empty?
       ar = []
       ar << coordinates
       ar << basic_moves.first
       ar = ar.transpose
       ar.map! { |a| a.sum }
-      @all_moves_available << ar if within_board?(ar)
+      all_moves << ar if within_board?(ar)
       basic_moves.shift
     end
-    all_moves_available
+    all_moves
   end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,25 +1,45 @@
 require 'knight'
 
 RSpec.describe Knight do
-  describe '#all_moves_calculation' do
+  describe '#all_moves_available' do
     context 'given [8, 8] as coordinates' do
       it 'returns [[7, 6], [6, 7]]' do
-        expect(Knight.new([8, 8]).all_moves_calculation).to eq([[7, 6], [6, 7]])
+        expect(Knight.new.all_moves_available([8, 8])).to eq([[7, 6], [6, 7]])
       end
     end
     context 'given [4, 4] as coordinates' do
       it 'returns [[5, 6], [6, 5], [2, 5], [3, 6], [6, 3], [5, 2], [3, 2], [2, 3]]' do
-        expect(Knight.new([4, 4]).all_moves_calculation).to eq([[5, 6], [6, 5], [2, 5], [3, 6], [6, 3], [5, 2], [3, 2], [2, 3]])
+        expect(Knight.new.all_moves_available([4, 4])).to eq([[5, 6], [6, 5], [2, 5], [3, 6], [6, 3], [5, 2], [3, 2], [2, 3]])
       end
     end
     context 'given [1, 1] as coordinates' do
       it 'returns [[2, 3], [3, 2]]' do
-        expect(Knight.new([1, 1]).all_moves_calculation).to eq([[2, 3], [3, 2]])
+        expect(Knight.new.all_moves_available([1, 1])).to eq([[2, 3], [3, 2]])
       end
     end
     context 'given [4, 8] as coordinates' do
       it 'returns [[6, 7], [5, 6], [3, 6], [2, 7]]' do
-        expect(Knight.new([4, 8]).all_moves_calculation).to eq([[6, 7], [5, 6], [3, 6], [2, 7]])
+        expect(Knight.new.all_moves_available([4, 8])).to eq([[6, 7], [5, 6], [3, 6], [2, 7]])
+      end
+    end
+  end
+
+  describe "#knight_moves" do 
+    context "when given [0,0],[1,2]" do
+      it "returns [[0,0],[1,2]]" do
+        expect(Knight.new.knight_moves([0,0],[1,2])).to eq([[0,0],[1,2]])
+      end
+    end
+
+    context "when given [0,0],[3,3]" do
+      it "returns [[0,0],[1,2],[3,3]]" do
+        expect(Knight.new.knight_moves([0,0],[1,2])).to eq([[0,0],[1,2],[3,3]])
+      end
+    end
+
+    context "when given [3,3],[0,0]" do
+      it "returns [[3,3],[1,2],[0,0]]" do
+        expect(Knight.new.knight_moves([3,3],[0,0])).to eq([[3,3],[1,2],[0,0]])
       end
     end
   end
